7500d882f178c21d0c32eced5e158c75460f9fb0
Faith Rose <70714890+F-rose@users.noreply.github.com>
2023-12-04 22:56:22 +0000

    init fix

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -10,7 +10,7 @@ def create_student(admin,studentID, firstname, lastname,password, contact, stude
 
 
 def create_staff(staffID, firstname, lastname, password, email, teachingExperience):
-		new_staff = Admin.addStaff(staffID, firstname=firstname, lastname=lastname, password=password, email=email, teachingExperience=teachingExperience)
+		new_staff = Admin.addStaff(staffID=staffID, firstname=firstname, lastname=lastname, password=password, email=email, teachingExperience=teachingExperience)
 		if new_staff:
 			return new_staff
 		return None
